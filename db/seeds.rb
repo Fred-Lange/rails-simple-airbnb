dec2a0366cd68e08c84d36a0a9863811029e0e6e
Fred Lange <pro.frederic.lange@gmail.com>
2022-02-16 16:27:23 +0100

    init de l appli railas avec modele, seed, controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 6 fake flats...'
+
+6.times do
+  flat = Flat.create!(
+    name: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 4),
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: Faker::Lorem.sentence(word_count: 8),
+    price_per_night: rand(70..200),
+    number_of_guests: rand(2..6)
+  )
+end
+
+puts 'Finished!'
