aa69b6b9faa682203abf6e20035c0bd228de9df3
Fred Lange <pro.frederic.lange@gmail.com>
2022-02-16 22:35:55 +0100

    debut de l exercice airbnb

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,29 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new # needed to instantiate the form_for
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render "new"
+    end
+    # no need for app/views/flats/create.html.erb
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
